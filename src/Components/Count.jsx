9904c2d757f6b21ccb1a24e4784010ef536ebc07
Okedo01 <eliahmwelangi01@gmail.com>
2026-05-06 15:20:18 +0300

    React State

--- src/Components/Count.jsx
+++ src/Components/Count.jsx
@@ -2,20 +2,35 @@ import { useState } from "react"
 
 export default function Count() {
     const [count, setCount ] = useState(0);
+    const [incrementBy, setIncrementBy ] = useState(1);
 
    function increment() {
-    setCount(count+1);
+    setCount(count+incrementBy);
    }
 
    function decrement() {
-    setCount(count-1);
+    setCount(count-incrementBy);
+   }
+
+   function increase() {
+    setIncrementBy(incrementBy+1);
+   }
+   function decrease() {
+    setIncrementBy(incrementBy-1);
    }
 
     return (
+        <>
         <div>
             <h1>The count value is: {count}</h1>
             <button onClick={increment}>Increment</button>
             <button onClick={decrement}>decrement</button>
         </div>
+        <div>
+            <h1>Increment count by: {incrementBy}</h1>
+            <button onClick={increase}>IncrementBy</button>
+            <button onClick={decrease}>DecrementBy</button>
+        </div>
+        </>
     )
 }
